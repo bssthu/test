47132b9139ee15a48b612a4bfc7d1ba98f2d86dc
bssthu <bssthu@users.noreply.github.com>
2016-12-11 11:25:58 +0800

    init project with ui

---
+++ lang_finder/Finder.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace lang_finder
+{
+    partial class Finder
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Finder));
+            this.textBoxText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.checkBoxRegex = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listViewResult = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // textBoxText
+            // 
+            this.textBoxText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxText.Location = new System.Drawing.Point(47, 12);
+            this.textBoxText.Name = "textBoxText";
+            this.textBoxText.Size = new System.Drawing.Size(441, 21);
+            this.textBoxText.TabIndex = 0;
+            this.textBoxText.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "原文";
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSearch.Location = new System.Drawing.Point(494, 10);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 2;
+            this.buttonSearch.Text = "搜索";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxRegex
+            // 
+            this.checkBoxRegex.AutoSize = true;
+            this.checkBoxRegex.Location = new System.Drawing.Point(14, 39);
+            this.checkBoxRegex.Name = "checkBoxRegex";
+            this.checkBoxRegex.Size = new System.Drawing.Size(84, 16);
+            this.checkBoxRegex.TabIndex = 3;
+            this.checkBoxRegex.Text = "正则表达式";
+            this.checkBoxRegex.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.label2.Location = new System.Drawing.Point(105, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(413, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "模糊匹配，例如\".*\"表示任意（不含引号），\"^\"表示文本开始，\"$\"表示结束";
+            // 
+            // listViewResult
+            // 
+            this.listViewResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listViewResult.Location = new System.Drawing.Point(14, 61);
+            this.listViewResult.Name = "listViewResult";
+            this.listViewResult.Size = new System.Drawing.Size(555, 411);
+            this.listViewResult.TabIndex = 5;
+            this.listViewResult.UseCompatibleStateImageBehavior = false;
+            this.listViewResult.View = System.Windows.Forms.View.Details;
+            // 
+            // Finder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 484);
+            this.Controls.Add(this.listViewResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBoxRegex);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxText);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Finder";
+            this.Text = "ESO 文本搜索";
+            this.Load += new System.EventHandler(this.Finder_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.CheckBox checkBoxRegex;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView listViewResult;
+    }
+}
+
---
+++ lang_finder/Finder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lang_finder
+{
+    public partial class Finder : Form
+    {
+        public Finder()
+        {
+            InitializeComponent();
+        }
+
+        private void Finder_Load(object sender, EventArgs e)
+        {
+            listViewResult.GridLines = true;
+            listViewResult.Columns.Add("类型", 80);
+            listViewResult.Columns.Add("编号", 160);
+            listViewResult.Columns.Add("原文", 400);
+        }
+    }
+}
